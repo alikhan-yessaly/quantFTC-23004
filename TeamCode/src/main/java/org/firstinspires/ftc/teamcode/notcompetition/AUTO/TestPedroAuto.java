ec13bd3145c5bee222d5e1a716063d057db486ac
Alikhan <alikhan.yessaly@quantum.edu.kz>
2024-11-06 19:42:14 +0500

    Updated Pedro Path constants. Added sample auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/notcompetition/AUTO/TestPedroAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/notcompetition/AUTO/TestPedroAuto.java
@@ -6,14 +6,14 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-import org.firstinspires.ftc.teamcode.utils.ServoPose;
-import org.firstinspires.ftc.teamcode.utils.ServoPoseFollower;
 import org.firstinspires.ftc.teamcode.pedroPathing.follower.Follower;
 import org.firstinspires.ftc.teamcode.pedroPathing.localization.Pose;
 import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.BezierCurve;
 import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.PathChain;
 import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.Point;
 import org.firstinspires.ftc.teamcode.pedroPathing.util.Timer;
+import org.firstinspires.ftc.teamcode.utils.ServoPose;
+import org.firstinspires.ftc.teamcode.utils.ServoPoseFollower;
 
 import java.util.Arrays;
 import java.util.List;
@@ -21,13 +21,16 @@ import java.util.List;
 @Autonomous(name = "TestPedroAuto", group = "Autonomous")
 public class TestPedroAuto extends OpMode {
 
+    private DcMotor armLift1, armLift2;
+
     // Private enum to define different autonomous states
     @SuppressWarnings("unused")
     private enum AutoState {
         START,
         FIRST_POSE,
-        DUMMY_PATH,
+        LIFT_UP,
         SECOND_POSE,
+        LIFT_DOWN,
         FINAL_POSE,
         COMPLETE
     }
@@ -42,14 +45,7 @@ public class TestPedroAuto extends OpMode {
     // Constants for starting pose
     private static final Pose START_POSE = new Pose(144 - (63 + 72), 12 + 72, 0);
 
-    DcMotor armLift1 = hardwareMap.dcMotor.get("lift1");
-    DcMotor armLift2 = hardwareMap.dcMotor.get("lift2");
-    armLift1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-    armLift2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-    armLift1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-    armLift2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-    armLift2.setDirection(DcMotorSimple.Direction.REVERSE);
 
 
     public void buildPaths() {
@@ -82,7 +78,7 @@ public class TestPedroAuto extends OpMode {
 
     public void defineInitialServoPoses(HardwareMap hardwareMap) {
         List<ServoPose> initialPoses = Arrays.asList(
-                new ServoPose(0.7, 0.9, 0.7, 1.0, 0.0, 1000)//,
+                new ServoPose(0, 0, 0, 0.5, 0.45, 1000)//,
 //                new ServoPose(1.0, 0.5, 0.5, 0.5, 1.0, 1500)
         );
         servoPoseFollower = new ServoPoseFollower(hardwareMap, initialPoses);
@@ -90,10 +86,16 @@ public class TestPedroAuto extends OpMode {
 
     public void defineSecondPoseSequence() {
         List<ServoPose> secondPoses = Arrays.asList(
-                new ServoPose(0.5, 0.6, 0.6, 0.9, 0.0, 1200),
-                new ServoPose(1.0, 0.8, 0.8, 1.0, 1.0, 1500)
+                new ServoPose(1, 1, 0.5, 0.5, 0.45, 1200)
+                //new ServoPose(1.0, 0.8, 0.8, 1.0, 1.0, 1500)
         );
         servoPoseFollower.setPoseSequence(secondPoses); // Load the new sequence
+    }
+    public void defineThirdPoseSequence() {
+        List<ServoPose> ThirdPoses = Arrays.asList(
+                new ServoPose(1, 1, 0.5, 0.5, 0.0, 1200)
+        );
+        servoPoseFollower.setPoseSequence(ThirdPoses); // Load the new sequence
     }
 
     @Override
@@ -101,6 +103,15 @@ public class TestPedroAuto extends OpMode {
         pathTimer = new Timer();
         opmodeTimer = new Timer();
         scanTimer = new Timer();
+
+        armLift1 = hardwareMap.dcMotor.get("lift1");
+        armLift2 = hardwareMap.dcMotor.get("lift2");
+        armLift1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armLift2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        armLift1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        armLift2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        armLift2.setDirection(DcMotorSimple.Direction.REVERSE);
 
         follower = new Follower(hardwareMap);
         follower.setStartingPose(START_POSE);
@@ -130,19 +141,31 @@ public class TestPedroAuto extends OpMode {
 
         switch (currentState) {
             case START:
-                follower.followPath(firstCycle); // Start path following
-                servoPoseFollower.start();       // Start servo pose follower
+                defineInitialServoPoses(hardwareMap);
+                servoPoseFollower.start();
                 break;
             case FIRST_POSE:
-                defineInitialServoPoses(hardwareMap);
-                servoPoseFollower.start();       // Start new servo sequence
+                follower.followPath(firstCycle);
                 break;
-            case DUMMY_PATH:
-//                follower.followPath(dummyPath);  // Start dummy path after first pose
+            case LIFT_UP:
+                armLift1.setTargetPosition(-1000);
+                armLift2.setTargetPosition(-1000);
+                armLift1.setPower(1);
+                armLift2.setPower(1);
+                armLift1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                armLift2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
                 break;
             case SECOND_POSE:
-//                defineSecondPoseSequence();
-//                servoPoseFollower.start();
+                defineSecondPoseSequence();
+                servoPoseFollower.start();
+                break;
+            case LIFT_DOWN:
+                armLift1.setTargetPosition(-1000);
+                armLift2.setTargetPosition(-1000);
+                armLift1.setPower(1);
+                armLift2.setPower(1);
+                armLift1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                armLift2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
                 break;
             case FINAL_POSE:
                 // Define any final poses or actions here
@@ -162,37 +185,29 @@ public class TestPedroAuto extends OpMode {
         // State transitions based on follower position and servo pose completion
         switch (currentState) {
             case START:
-                if (follower.isCloseEnoughToEnd()) {
-                    setPathState(AutoState.FIRST_POSE); // Move to FIRST_POSE checkpoint
-                }
+                setPathState(AutoState.START);// Transition to DUMMY_PATH after FIRST_POSE
                 break;
 
             case FIRST_POSE:
-                if (servoPoseFollower.isComplete()) {
-                    setPathState(AutoState.DUMMY_PATH); // Transition to DUMMY_PATH after FIRST_POSE
-                }
-                break;
-
-            case DUMMY_PATH:
                 if (follower.isCloseEnoughToEnd()) {
-                    setPathState(AutoState.SECOND_POSE); // Move to SECOND_POSE after completing dummy path
+                    setPathState(AutoState.FIRST_POSE); // Move to SECOND_POSE after completing dummy path
                 }
                 break;
 
-            case SECOND_POSE:
-                if (servoPoseFollower.isComplete()) {
-                    setPathState(AutoState.FINAL_POSE); // Move to FINAL_POSE checkpoint
-                }
+            case LIFT_UP:
+                setPathState(AutoState.LIFT_UP);
+                break;
+
+            case LIFT_DOWN:
+                setPathState(AutoState.LIFT_DOWN); // Move to FINAL_POSE checkpoint
                 break;
 
             case FINAL_POSE:
-                if (servoPoseFollower.isComplete()) {
-                    setPathState(AutoState.COMPLETE); // Finalize actions
-                }
                 break;
 
             case COMPLETE:
                 telemetry.addData("Status", "Sequence Complete");
+                telemetry.update();
                 break;
         }
 
